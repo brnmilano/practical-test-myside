692c3ff9ce34030250daeff7f071bef86e632848
Bruno <brn.milano@hotmail.com>
2024-09-07 00:02:16 -0300

    Adição do parâmetro page na URL com o número da página atual

--- src/hooks/useCommon.tsx
+++ src/hooks/useCommon.tsx
@@ -1,4 +1,6 @@
 import { AddressProps } from "@/types/address";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import {
   Dispatch,
   ReactNode,
@@ -16,7 +18,7 @@ interface CommonContextData {
   loading: boolean;
   setLoading: Dispatch<SetStateAction<boolean>>;
   currentPage: number;
-  setCurrentPage: Dispatch<SetStateAction<number>>;
+  setCurrentPage: (pageNumber: number) => void;
   shippingAddress: AddressProps;
   setShippingAddress: Dispatch<SetStateAction<AddressProps>>;
 }
@@ -24,11 +26,34 @@ interface CommonContextData {
 export const CommonContext = createContext({} as CommonContextData);
 
 function CommonProvider({ children }: useCommonProps) {
+  const searchParams = useSearchParams();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const [shippingAddress, setShippingAddress] = useState<AddressProps>(
     {} as AddressProps
   );
+
+  const currentPage = Number(searchParams.get("page") || 1);
+
+  /**
+   * Atualiza a URL da página atual adicionando o valor do parâmetro para "page".
+   *
+   * @param {number} pageNumber - O número da página que deve ser definida na URL.
+   *
+   * @example Se a página atual for 2, a URL será: /home?page=2
+   *
+   * @see https://nextjs.org/docs/app/api-reference/functions/use-search-params#updating-searchparams
+   */
+  const setCurrentPage = (pageNumber: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    params.set("page", pageNumber.toString());
+
+    router.push(pathname + "?" + params.toString());
+  };
 
   return (
     <CommonContext.Provider
