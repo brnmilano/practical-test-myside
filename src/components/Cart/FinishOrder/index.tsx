01407a25370535d27ddd4178263d3dd191226923
Bruno <brn.milano@hotmail.com>
2024-10-20 22:46:38 -0300

    test

--- src/components/Cart/FinishOrder/index.tsx
+++ src/components/Cart/FinishOrder/index.tsx
@@ -54,7 +54,7 @@ export default function FinishOrder() {
                           className={styles.removeProduct}
                           key={`${product.id} ${index}`}
                         >
-                          Remover
+                          Removerr
                         </div>
                       </div>
                     </div>
